7cd94940fd4dd078cc0c7ad69de44a5a8b212ffc
Igor <polus79@mail.ru>
2018-04-25 11:28:28 +0500

    rutor update mirirors

--- src/main/java/news/ruTor/RutorMirrors.java
+++ src/main/java/news/ruTor/RutorMirrors.java
@@ -4,10 +4,12 @@ package news.ruTor;
  * Created by golit on 05.06.2017.
  */
 public enum RutorMirrors {
-    rutor2("http://rutor.info"),
     rutor1("http://rus-film.org"),
+    rutor2("http://rutor.info"),
     rutor3("http://rutor.is"),
-    rutor4("http://fast-bit.org");
+    rutor4("http://fast-bit.org"),
+    rutor5("http://zerkalo-rutor.org"),
+    rutor6("http://free-tor.org");
 
     private final String name;
 
