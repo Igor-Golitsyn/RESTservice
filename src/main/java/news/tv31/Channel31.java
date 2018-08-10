ce41d86b142ae5a1a6812e31497a586d9bf27eb1
Igor <polus79@mail.ru>
2018-08-10 12:03:09 +0500

    update Channel31

--- src/main/java/news/tv31/Channel31.java
+++ src/main/java/news/tv31/Channel31.java
@@ -23,22 +23,21 @@ import java.util.HashSet;
  * Created by golit on 13.06.2017.
  */
 public class Channel31 implements Model {
-    private final DateFormat FORMAT = new SimpleDateFormat("dd.MM.yy hh:mm");
+    private final DateFormat FORMAT = new SimpleDateFormat("dd MMMM yyyy - hh:mm");
 
     @Override
     public NewsItem[] getItems(String searchWord) {
         Document document = getDocument(ConstantManager.TV31NEWSURL);
         if (document == null) return new NewsItem[0];
         ArrayList<NewsItem> newsItems = new ArrayList<>();
-        Elements arhive = document.getElementsByClass("archive row-fluid");
-        Elements news = arhive.first().getElementsByClass("text");
+        Elements news = document.getElementsByClass("card border-on_grey shadow");
         for (Element element : news) {
-            String name = element.getElementsByClass("title").first().text();
-            String url = element.getElementsByClass("title").first().child(0).absUrl("href");
+            String name = element.child(0).text();
+            String url = element.child(1).absUrl("href");
             Date date;
             try {
                 synchronized (FORMAT) {
-                    date = FORMAT.parse(element.getElementsByClass("date").first().text());
+                    date = FORMAT.parse(element.getElementsByClass("text-muted").first().text());
                 }
             } catch (ParseException e) {
                 date = new Date();
@@ -51,7 +50,6 @@ public class Channel31 implements Model {
                 return Long.compare(o2.getDate(),o1.getDate());
             }
         });
-        System.out.println(newsItems);
         return newsItems.toArray(new NewsItem[newsItems.size()]);
     }
 
@@ -60,15 +58,15 @@ public class Channel31 implements Model {
         Document document = getDocument(pageRequest.getUrl());
         if (document == null)
             return new NewsPage(ConstantManager.ERRORDOWNLOADPAGE, new HashSet<>(), "", "", "", "", "");
-        Element element = document.getElementsByClass("text article_text").first();
-        String head = element.getElementsByTag("p").first().text();
-        element.getElementsByTag("p").first().remove();
-        Elements imagesEl = element.getElementsByTag("img");
         HashSet<String> setUrlImgs = new HashSet<>();
-        for (Element im:imagesEl){
-            setUrlImgs.add(im.absUrl("src"));
+        String head = document.getElementsByClass("text-center").first().text();
+        String text = "";
+        Element mediacontainer = document.getElementsByClass("media-container").first().child(0);
+        setUrlImgs.add(mediacontainer.absUrl("src"));
+        Elements ppp = document.getElementsByTag("p");
+        for (Element el : ppp) {
+            text = text + el.text() + "\n";
         }
-        String text = element.text();
         return new NewsPage(head,setUrlImgs,text,ConstantManager.OPENINBRAUZER,pageRequest.getUrl(),"","");
     }
 
@@ -81,9 +79,17 @@ public class Channel31 implements Model {
         }
         return document;
     }
-    /*public static void main(String[] args) {
-        PageRequest pageRequest = new PageRequest("https://31tv.ru/novosti/yuzhnouralec-otkazalsya-oplachivat-shtraf--nazyvaya-sebya-grazhdaninom-sssr-i-zhivorozhdennym-muzhchinoy-1-8-2017-143020.html", "");
-        NewsPage newsPage=new Channel31().getNewsPage(pageRequest);
-        System.out.println(newsPage);
-    }*/
+
+    public static void main(String[] args) {
+        Channel31 channel31 = new Channel31();
+        NewsItem[] ooo = channel31.getItems("");
+        System.out.println(ooo.length);
+        for (NewsItem o : ooo) {
+            System.out.println(o);
+            System.out.println(new Date(o.getDate()));
+        }
+        PageRequest pageRequest = new PageRequest(ooo[ooo.length-1].getLink(),ooo[0].getName());
+        NewsPage page = channel31.getNewsPage(pageRequest);
+        System.out.println(page);
+    }
 }
